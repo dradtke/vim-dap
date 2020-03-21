d125b0b14638cdfe6fae4963cf436c3a4e4f0f5b
Damien Radtke <git@damienradtke.com>
2020-03-20 21:19:02 -0500

    Add release helper script

---
+++ evaluator/README.md
@@ -0,0 +1,2 @@
+This directory contains the source code for the evaluator console, as well as
+a helper script for continuously reading from a named pipe in vim.
---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if ! command -v go >/dev/null; then
+	echo "go not found, can't create a release"
+	exit 1
+fi
+
+eval "$(go env)"
+echo "Building debug console binary for ${GOOS}/${GOARCH}..."
+binary_name="console-${GOOS}-${GOARCH}"
+
+mkdir -p bin
+(cd evaluator/console && go build -o "../../bin/${binary_name}")
+
+echo ""
+echo "Binaries available in bin/"
